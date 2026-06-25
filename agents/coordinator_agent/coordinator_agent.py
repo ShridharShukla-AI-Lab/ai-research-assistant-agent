df56e7a61b672498719b4c2d990c86571f3a831e
shridhar_shukla <sds.gplus@gmail.com>
2026-06-25 21:33:25 +0530

    Day 3: Coordinator agent routing working

--- agents/coordinator_agent/coordinator_agent.py
+++ agents/coordinator_agent/coordinator_agent.py
@@ -1,4 +1,5 @@
 #coordinator_agent.py
+
 import os
 from dotenv import load_dotenv
 #import google.generativeai as genai   - older version / switch to newer version below
@@ -10,10 +11,51 @@ client = genai.Client(
     api_key=os.getenv("GEMINI_API_KEY")
 )
 
-response = client.models.generate_content(
-    model="gemini-2.5-flash",
-    contents="Say hello"
-)
+def choose_agent(user_query):
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=f"""
+        You are a router .
+        
+        Choose exactly one:
+        
+        TOOL
+        RESEARCH
+        
+        Question: {user_query}
+        
+        """
+    )
 
-print(response.text)
+    return response.text.strip()
 
+
+user_input = input("Ask something: ")
+#agent_choice = choose_agent(user_input)
+#print("Chosen Agent:", agent_choice)
+
+agent_choice = choose_agent(user_input)
+print("Chosen Agent:", agent_choice)
+#####
+
+# coordinator agent manages calling research agent and tool agent
+# #create coordinator logic
+
+from agents.research_agent.research_agent import research_topic
+from agents.tool_agent.tool_agent import ask_tool_agent
+
+#question = input("Ask something: ")
+#above question is shifted to top to implement agent choice logic
+
+# question variable is now replaced with user_input
+#if any(word in question.lower() for word in ["calculate", "multiply", "divide", "+", "-", "*", "/"]):
+#    result = ask_tool_agent(question)
+if any(word in user_input.lower() for word in ["calculate", "multiply", "divide", "+", "-", "*", "/"]):
+    result = ask_tool_agent(user_input)
+    
+else:
+    result = research_topic(user_input)
+    
+    
+print("\nFinal Answer:\n")
+print(result)
--- agents/tool_agent/tool_agent.py
+++ agents/tool_agent/tool_agent.py
@@ -24,63 +24,75 @@ def calculator(expression):
         return f"Error: {e}"
         
      
-question = input("Ask a question: ")
+#question = input("Ask a question: ")
 
-response = client.models.generate_content(
-    model="gemini-2.5-flash",
-    contents=f"""
-You are an assistant.
 
-If the user asks a mathematical calculation, reply 
- ONLY in this format:
- 
-CALCULATE: expression
+# ##make too agent callable, wrap results into function
 
-Examples:
-CALCULATE: 25*4
-CALCULATE: (100+50)/3
-
-otherwise answer normally.
-
-User Question:
-{question}
-"""
-)
-
-model_reply = response.text.strip()
-
-#print("\nModel Reply:")
-#print(model_reply)
-time.sleep(15)
-if model_reply.startswith("CALCULATE:"):
-    expression = model_reply.replace("CALCULATE:","").strip()
+def ask_tool_agent(question):
     
-    result = calculator(expression)
-    
-    final_response = client.models.generate_content(
+    response = client.models.generate_content(
         model="gemini-2.5-flash",
         contents=f"""
-    User asked:
-    
+    You are an assistant.
+
+    If the user asks a mathematical calculation, reply 
+     ONLY in this format:
+     
+    CALCULATE: expression
+
+    Examples:
+    CALCULATE: 25*4
+    CALCULATE: (100+50)/3
+
+    otherwise answer normally.
+
+    User Question:
     {question}
-    
-    Calculator result:
-    
-    {result}
-    
-    Provide a helpful final answer.
     """
     )
-    
-    print("\nFinal Answer:")
-    print(final_response.text)
-    
-#    print("\nTool Used: Calculator")
-#    print("Expression:", expression)
-#    print("Result:", result)
 
+    model_reply = response.text.strip()
 
+    #print("\nModel Reply:")
+    #print(model_reply)
+    time.sleep(15)
+    if model_reply.startswith("CALCULATE:"):
+        expression = model_reply.replace("CALCULATE:","").strip()
+        
+        result = calculator(expression)
+        
+        final_response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents=f"""
+        User asked:
+        
+        {question}
+        
+        Calculator result:
+        
+        {result}
+        
+        Provide a helpful final answer.
+        """
+        )
+        
+    #    print("\nFinal Answer:")
+    #    print(final_response.text)
+        final_response = final_response.text
+        return final_response
+        
+    #    print("\nTool Used: Calculator")
+    #    print("Expression:", expression)
+    #    print("Result:", result)
 
-else:
-    print("\nDirect Answer:")
-    print(model_reply)
+    #else:
+    #    print("\nDirect Answer:")
+    #    print(model_reply)
+        
+    return model_reply
+        
+if __name__ == "__main__":
+    question = input("Ask a question: ")
+    result = ask_tool_agent(question)
+    print(result)
